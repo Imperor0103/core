49c551a8aa5266d4c16e95a5fc71e48badb09dbf
Imperor0103 <imperor0103@gmail.com>
2025-07-30 11:54:12 +0900

    Feat: RateDiscoundPolicy

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,26 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+
+public class RateDiscountPolicy implements  DiscountPolicy
+{
+    private int discountPercent = 10;
+
+
+    // ctrl + shift + t: 새로운 test 만들기
+    @Override
+    public int discount(Member member, int price)
+    {
+        if (member.getGrade() == Grade.VIP)
+        {
+            return price * discountPercent /  100;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+}
