40697900ae6d5922dfca935e3de42ba64d33e191
Imperor0103 <imperor0103@gmail.com>
2025-08-06 12:20:06 +0900

    Fix: Use local variable than shared field

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -3,15 +3,17 @@ package hello.core.singleton;
 // ctrl + shift + t -> Create New Test -> JUnit5 -> 새로운 이름의 클래스 생성 가능
 public class StatefulService
 {
-    private int price; // 상태를 유지하는 필드 10000 -> 20000
+//    private int price; // 상태를 유지하는 필드 10000 -> 20000
 
-    public void order(String name, int price)
+    public int order(String name, int price)
     {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price; // 여기가 문제!
-    }
-    public int getPrice()
-    {
+//        this.price = price; // 여기가 문제!
+
         return price;
     }
+//    public int getPrice()
+//    {
+//        return price;
+//    }
 }
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -19,18 +19,18 @@ class StatefulServiceTest
 
         // ctrl + shift + ↑↓: 해당 line을 화살표 방향으로 이동
         // ThreadA: A사용자가 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
 
         // ThreadB: B사용자가 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         // ThreadA: 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price); // 20000원 출력(문제발생)
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice); // 20000원 출력(문제발생)
         // price는 공유필드인데 특정 클라이언트가 값을 변경하는 것이 문제
         // 이러한 문제가 발생하지 않도록 sprind의 Bean은 항상 무상태(stateless)로 설계해야 한다
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig
