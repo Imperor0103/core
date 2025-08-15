07f34ea193924fec77d2c59bcc0f5a7a1283808c
Imperor0103 <imperor0103@gmail.com>
2025-08-15 17:46:56 +0900

    Test: Empty life cycle callback

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,34 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest
+{
+    @Test
+    public void lifeCycleTest()
+    {
+        // ctrl + b: 정의로 이동
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+
+        // ConfigurableApplicationContext에 정의된 close메서드로 닫아야 한다
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig
+    {
+        @Bean
+        public NetworkClient networkClient()
+        {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
