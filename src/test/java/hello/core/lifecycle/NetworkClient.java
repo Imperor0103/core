c2bbfcd8fb4fdb5b4b6ad3c42d53d389f5c659dc
Imperor0103 <imperor0103@gmail.com>
2025-08-16 20:45:15 +0900

    Refactor: @PostContruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -2,6 +2,10 @@ package hello.core.lifecycle;
 
 // 테스트를 위한 가짜 네트워크
 
+// java EE가 Eclipse 재단으로 넘어가면서 패키지가 javax -> jakarta로 바뀌었다
+// 기능 차이는 없다
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 public class NetworkClient
 {
@@ -33,6 +37,7 @@ public class NetworkClient
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     // property의 초기화(의존관계 주입)가 끝나면 호출되는 메서드
     public void init() throws Exception
     {
@@ -41,6 +46,7 @@ public class NetworkClient
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close()
     {
         System.out.println("NetworkClient.close");
